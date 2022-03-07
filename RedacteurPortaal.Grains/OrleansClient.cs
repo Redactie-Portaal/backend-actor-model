d76ebbd50c52d9d3d8eaead7b0b40804fd1923b4
Joep van Diessen <joepvandiessen@gmail.com>
2022-03-07 13:14:36 +0100

    no message

--- RedacteurPortaal.Grains/OrleansClient.cs
+++ RedacteurPortaal.Grains/OrleansClient.cs
@@ -4,6 +4,6 @@ namespace RedacteurPortaal.Grains
 {
     public static class OrleansClient
     {
-        public static IClusterClient ClusterClient { get; set; }
+        public static IClusterClient ClusterClient { get; set; } 
     }
 }
