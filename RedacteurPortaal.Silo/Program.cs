3910256cdf3ec9cb2998e937a4045c8f114e755b
Joep van Diessen <joepvandiessen@gmail.com>
2022-03-07 10:34:15 +0100

    Changes

--- RedacteurPortaal.Silo/Program.cs
+++ RedacteurPortaal.Silo/Program.cs
@@ -1,6 +1,8 @@
-﻿using Orleans.Clustering.Kubernetes;
+﻿using Microsoft.Extensions.Logging;
+using Orleans.Clustering.Kubernetes;
 using Orleans.Configuration;
 using Orleans.Hosting;
+using System.Net;
 
 namespace RedacteurPortaal.Silo
 {
@@ -13,8 +15,8 @@ namespace RedacteurPortaal.Silo
                 Console.WriteLine("Starting silo...");
                 var host = await StartSilo();
                 Console.WriteLine("Press Enter to terminate...");
+                await Task.Delay(int.MaxValue);
                 Console.ReadLine();
-
                 await host.StopAsync();
             }
             catch (Exception ex)
@@ -25,17 +27,20 @@ namespace RedacteurPortaal.Silo
 
         private static async Task<ISiloHost> StartSilo()
         {
+
+            var clusterId = "test";
+            var serviceId = "test";
+
+            IPAddress.TryParse(Environment.GetEnvironmentVariable("HostIP"), out var ip);
             var builder = new SiloHostBuilder()
-                .UseKubeMembership()
-                .Configure<ClusterOptions>(c =>
-                {
-                    c.ClusterId = "Test";
-                    c.ServiceId = "TestService";
-                })
-                .Configure<EndpointOptions>(c =>
-                {
-                    c.AdvertisedIPAddress = System.Net.IPAddress.Loopback;
-                });
+                    .UseLocalhostClustering()
+                            .Configure<ClusterOptions>(options =>
+                            {
+                                options.ClusterId = clusterId;
+                                options.ServiceId = serviceId;
+                            })
+                            .ConfigureLogging(logging => logging.AddConsole())
+                            .ConfigureLogging(logging => logging.SetMinimumLevel(LogLevel.Warning));
 
             var host = builder.Build();
             await host.StartAsync();
