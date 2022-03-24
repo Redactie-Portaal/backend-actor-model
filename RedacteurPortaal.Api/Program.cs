eeab52727430ae29fb3d0269f66f2f5d53d40b3e
Joep van Diessen <joepvandiessen@gmail.com>
2022-03-24 12:15:10 +0100

    try

--- RedacteurPortaal.Api/Program.cs
+++ RedacteurPortaal.Api/Program.cs
@@ -16,7 +16,6 @@ await Host.CreateDefaultBuilder(args)
             // Use Redis for clustering & persistence
             var redisConnectionString = Environment.GetEnvironmentVariable("REDIS");
             var postgresqlConnString = Environment.GetEnvironmentVariable("POSTGRESQL");
-            siloBuilder.UseRedisClustering(options => options.ConnectionString = redisConnectionString);
             siloBuilder.AddAdoNetGrainStorage("OrleansStorage", options =>
             {
                 options.Invariant = "Npgsql";
