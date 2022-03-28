ab9e58892b248463dc2ab1bfe33e357c35486826
Chas1t <christianvandenbroek@kpnmail.nl>
2022-03-28 10:24:45 +0200

    Added test controller

--- RedacteurPortaal.Grains/TestGrain.cs
+++ RedacteurPortaal.Grains/TestGrain.cs
@@ -1,11 +1,5 @@
-﻿
-using Microsoft.Extensions.Logging;
+﻿using Microsoft.Extensions.Logging;
 using Orleans;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace RedacteurPortaal.Grains
 {
