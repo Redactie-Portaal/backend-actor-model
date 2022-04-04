d115886a9c6510c65c1527ee24ed807075527a54
Chas1t <christianvandenbroek@kpnmail.nl>
2022-04-04 14:22:17 +0200

    Changed dto

--- RedacteurPortaal.Api/Models/NewsItemDTO.cs
+++ RedacteurPortaal.Api/Models/NewsItemDTO.cs
@@ -8,41 +8,25 @@ namespace RedacteurPortaal.Api.Models;
 
 public class NewsItemDTO
 {
-    public NewsItemDTO(
-        string title,
-        string status,
-        string author,
-        string contactDetails,
-        string locationDetails,
-        string produtionDate,
-        string category,
-        string region)
+    public NewsItemDTO()
     {
-        this.Title = title ?? throw new ArgumentNullException(nameof(title));
-        this.Status = status;
-        this.Author = author ?? throw new ArgumentNullException(nameof(author));
-        this.ContactDetails = contactDetails ?? throw new ArgumentNullException(nameof(contactDetails));
-        this.LocationDetails = locationDetails ?? throw new ArgumentNullException(nameof(locationDetails));
-        this.ProdutionDate = produtionDate;
-        this.Category = category;
-        this.Region = region;
     }
 
-    public Guid Id { get; set; } = Guid.NewGuid();
+    public Guid? Id { get; set; } = Guid.NewGuid();
 
-    public string Title { get; }
+    public string? Title { get; }
 
-    public string Status { get; }
+    public string? Status { get; }
 
-    public string Author { get; }
+    public string? Author { get; }
 
-    public string ContactDetails { get; }
+    public string? ContactDetails { get; }
 
-    public string LocationDetails { get; }
+    public string? LocationDetails { get; }
 
-    public string ProdutionDate { get; }
+    public string? ProdutionDate { get; }
 
-    public string Category { get; }
+    public string? Category { get; }
 
-    public string Region { get; }
+    public string? Region { get; }
 }
