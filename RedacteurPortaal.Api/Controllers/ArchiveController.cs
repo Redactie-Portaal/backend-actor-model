70ef86c3aa72322398ec722196db86c7b414b93c
Emre <emre_ftw@live.nl>
2022-04-07 14:23:26 +0200

    archiefcontroller update
    laatste wijzigingen voltooid

--- RedacteurPortaal.Api/Controllers/ArchiveController.cs
+++ RedacteurPortaal.Api/Controllers/ArchiveController.cs
@@ -6,13 +6,13 @@ using RedacteurPortaal.Api.DTOs;
 namespace RedacteurPortaal.Api.Controllers;
 
 [ApiController]
-[Route("api/archive")]
+[Route("api/[controller]")]
 public class ArchiveController : Controller
 {
     private readonly IClusterClient client;
     private readonly ILogger logger;
 
-    public ArchiveController(IClusterClient client, ILogger logger)
+    public ArchiveController(IClusterClient client, ILogger<ArchiveController> logger)
     {
         this.client = client;
         this.logger = logger;
