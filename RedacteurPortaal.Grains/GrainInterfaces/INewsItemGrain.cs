c62b25b4f25def882e0c4d755d2ea6cf3642a969
Chas1t <christianvandenbroek@kpnmail.nl>
2022-04-07 14:41:46 +0200

    Hiding

--- RedacteurPortaal.Grains/GrainInterfaces/INewsItemGrain.cs
+++ RedacteurPortaal.Grains/GrainInterfaces/INewsItemGrain.cs
@@ -6,13 +6,13 @@ namespace RedacteurPortaal.Grains.GrainInterfaces;
 
 public interface INewsItemGrain : IManageableGrain<NewsItemModel>
 {
-    Task<NewsItemModel> Get();
+    new Task<NewsItemModel> Get();
 
     Task AddNewsItem(NewsItemModel newsitem);
 
     Task DeleteNewsItem(Guid guid);
 
-    Task Delete();
+    new Task Delete();
 
     Task Update(NewsItemUpdate update);
 }
