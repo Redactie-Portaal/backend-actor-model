c86691389e136cdb451a7dc4ffff64c2691dee8b
Dmitri <dmitri.lisokonov@gmail.com>
2022-04-07 14:57:43 +0200

    Added SourceGrain
    Added source grain, interface and model

---
+++ RedacteurPortaal.Grains/GrainInterfaces/ISourceGrain.cs
@@ -0,0 +1,20 @@
+﻿using Orleans;
+using RedacteurPortaal.DomainModels.NewsItem;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedacteurPortaal.Grains.GrainInterfaces;
+
+public interface ISourceGrain : IGrainWithGuidKey
+{
+    Task<Source> GetSource(Guid guid);
+
+    Task AddSource(Source source);
+
+    Task DeleteSource(Guid guid);
+
+    Task UpdateSource(Source source);
+}
---
+++ RedacteurPortaal.Grains/Grains/SourceGrain.cs
@@ -0,0 +1,43 @@
+﻿using Orleans.Runtime;
+using RedacteurPortaal.DomainModels.NewsItem;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedacteurPortaal.Grains.Grains;
+
+public class SourceGrain
+{
+    private readonly IPersistentState<Source> source;
+
+    public SourceGrain(
+        [PersistentState("source", "OrleansStorage")]
+        IPersistentState<Source> source)
+    {
+        this.source = source;
+    }
+
+    public Task<Source> GetSource(Guid guid)
+    {
+        return Task.FromResult(this.source.State);
+    }
+
+    public async Task AddSource(Source item)
+    {
+        this.source.State = item;
+        await this.source.WriteStateAsync();
+    }
+
+    public async Task DeleteSource(Guid guid)
+    {
+        await this.source.ClearStateAsync();
+    }
+
+    public async Task UpdateSource(Source item)
+    {
+        this.source.State = item;
+        await this.source.WriteStateAsync();
+    }
+}
