d6092f1c6c39fccaa38ac4e0f4b5a13e09de888a
Dmitri <dmitri.lisokonov@gmail.com>
2022-04-14 11:47:11 +0200

    Fixed add item grain

--- RedacteurPortaal.Api/DTOs/MediaItemDTO.cs
+++ RedacteurPortaal.Api/DTOs/MediaItemDTO.cs
@@ -4,34 +4,7 @@ using RedacteurPortaal.DomainModels.Shared;
 namespace RedacteurPortaal.Api.DTOs
 {
     public abstract class MediaItemDTO
-    {
-        protected MediaItemDTO(
-            Guid id,
-            string title,
-            string folder,
-            DateTime republishDate,
-            string rights,
-            string camera,
-            string lastWords,
-            string proxyFile,
-            string presentation,
-            Location location,
-            string format,
-            Uri mediaLocation)
-        {
-            this.Title = title ?? throw new ArgumentNullException(nameof(title));
-            this.Folder = folder ?? throw new ArgumentNullException(nameof(folder));
-            this.RepublishDate = republishDate;
-            this.Rights = rights ?? throw new ArgumentNullException(nameof(rights));
-            this.Camera = camera ?? throw new ArgumentNullException(nameof(camera));
-            this.LastWords = lastWords ?? throw new ArgumentNullException(nameof(lastWords));
-            this.ProxyFile = proxyFile ?? throw new ArgumentNullException(nameof(proxyFile));
-            this.Presentation = presentation ?? throw new ArgumentNullException(nameof(presentation));
-            this.Location = location ?? throw new ArgumentNullException(nameof(location));
-            this.Format = format ?? throw new ArgumentNullException(nameof(format));
-            this.MediaLocation = mediaLocation ?? throw new ArgumentNullException(nameof(mediaLocation));
-        }
-        
+    { 
         public Guid Id { get; set; }
 
         public string Title { get; set; }
