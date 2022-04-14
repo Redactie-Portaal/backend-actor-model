6fb11abd7b87977cbc7d17310886dd85ab22f6ce
okan piri <384909@student.fontys.nl>
2022-04-14 11:22:54 +0200

    Created dto's

---
+++ RedacteurPortaal.Api/DTOs/LocationDto.cs
@@ -0,0 +1,21 @@
+﻿namespace RedacteurPortaal.Api.DTOs
+{
+    public class LocationDto
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string City { get; set; }
+
+        public string Province { get; set; }
+
+        public string Street { get; set; }
+
+        public string Zip { get; set; }
+
+        public decimal Latitude { get; set; }
+
+        public decimal Longitude { get; set; }
+    }
+}
