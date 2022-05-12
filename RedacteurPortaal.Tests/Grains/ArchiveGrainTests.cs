33415edd97c72706a4d131a667edeb37246db2d3
Emre <emre_ftw@live.nl>
2022-05-12 11:49:44 +0200

    Archive tests
    Delete, Get one & update functions are tested

--- RedacteurPortaal.Tests/Grains/ArchiveGrainTests.cs
+++ RedacteurPortaal.Tests/Grains/ArchiveGrainTests.cs
@@ -31,7 +31,7 @@ public class ArchiveGrainTests
         var toSaveArchive = new ArchiveModel(guid, "Title", "Label", new List<MediaPhotoItem> { }, new List<MediaVideoItem> { }, new List<MediaAudioItem> { }, new List<NewsItemModel> { }, DateTime.UtcNow, new List<string> { });
 
         var archiveGrain = this._cluster.GrainFactory.GetGrain<IArchiveGrain>(guid);
-        await archiveGrain.Update(toSaveArchive);
+        await archiveGrain.CreateArchive(toSaveArchive);
 
         var updatedArchive = await this._cluster.GrainFactory.GetGrain<IArchiveGrain>(guid).Get();
 
@@ -42,6 +42,57 @@ public class ArchiveGrainTests
         Assert.Equal(toSaveArchive.MediaVideoItems, updatedArchive.MediaVideoItems);
         Assert.Equal(toSaveArchive.MediaPhotoItems, updatedArchive.MediaPhotoItems);
         Assert.Equal(toSaveArchive.Scripts, updatedArchive.Scripts);
+    }
+
+    [Fact]
+    public async Task CanGetASingularArchiveCorrectly()
+    {
+        var guid = Guid.NewGuid();
+
+        var toSaveArchive = new ArchiveModel(guid, "Title", "Label", new List<MediaPhotoItem> { }, new List<MediaVideoItem> { }, new List<MediaAudioItem> { }, new List<NewsItemModel> { }, DateTime.UtcNow, new List<string> { });
+        var archiveGrain = this._cluster.GrainFactory.GetGrain<IArchiveGrain>(guid);
+        await archiveGrain.CreateArchive(toSaveArchive);
+
+        var archiveInGrain = await this._cluster.GrainFactory.GetGrain<IArchiveGrain>(guid).Get();
+
+        Assert.NotNull(archiveInGrain);
+    }
+
+    [Fact]
+    public async Task UpdateArchiveCorrectly()
+    {
+        var guid = Guid.NewGuid();
+
+        var toSaveArchive = new ArchiveModel(guid, "Title", "Label", new List<MediaPhotoItem> { }, new List<MediaVideoItem> { }, new List<MediaAudioItem> { }, new List<NewsItemModel> { }, DateTime.UtcNow, new List<string> { });
+        var archiveGrain = this._cluster.GrainFactory.GetGrain<IArchiveGrain>(guid);
+        await archiveGrain.CreateArchive(toSaveArchive);
+
+        var newerArchive = new ArchiveModel(guid, "Newer Title", "Newer Label", new List<MediaPhotoItem> { }, new List<MediaVideoItem> { }, new List<MediaAudioItem> { }, new List<NewsItemModel> { }, DateTime.UtcNow, new List<string> { });
+
+        await archiveGrain.Update(newerArchive);
+
+
+        var archiveInGrain = await this._cluster.GrainFactory.GetGrain<IArchiveGrain>(guid).Get();
+        Assert.NotEqual(toSaveArchive.Title, archiveInGrain.Title);
+        Assert.NotEqual(toSaveArchive.Label, archiveInGrain.Label);
+
+    }
+
+    [Fact]
+    public async Task CanDeleteArchiveCorrectly()
+    {
+        var guid = Guid.NewGuid();
+
+        var toSaveArchive = new ArchiveModel(guid, "Title", "Label", new List<MediaPhotoItem> { }, new List<MediaVideoItem> { }, new List<MediaAudioItem> { }, new List<NewsItemModel> { }, DateTime.UtcNow, new List<string> { });
+        var archiveGrain = this._cluster.GrainFactory.GetGrain<IArchiveGrain>(guid);
+        await archiveGrain.CreateArchive(toSaveArchive);
+
+        await this._cluster.GrainFactory.GetGrain<IArchiveGrain>(guid).Delete();
+        var archiveInGrain = await this._cluster.GrainFactory.GetGrain<IArchiveGrain>(guid).Get();
+
+        Assert.Equal("00000000-0000-0000-0000-000000000000", Convert.ToString(archiveInGrain.Id));
+        Assert.Null(archiveInGrain.Title);
+        Assert.Null(archiveInGrain.Label);
     }
 }
 
